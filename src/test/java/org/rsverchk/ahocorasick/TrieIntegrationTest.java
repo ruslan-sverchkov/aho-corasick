fceb11138ad36ff57af0b2fc6b43c36b51b484b3
ruslan.sverchkov <ruslan.sverchkov@corp.mail.ru>
2016-08-06 20:56:49 +0300

    create project structure

--- src/test/java/org/rsverchk/ahocorasick/TrieIntegrationTest.java
+++ src/test/java/org/rsverchk/ahocorasick/TrieIntegrationTest.java
@@ -18,7 +18,116 @@ import static org.junit.Assert.*;
 public class TrieIntegrationTest {
 
     @Test
-    public void test() {
+    public void testNoMatches() {
+        TrieBuilder<String> builder = new TrieBuilder<>();
+        Trie<String> trie = builder.addCharSequence("text", "text")
+                .build();
+
+        Set<Triple<Integer, Integer, String>> set = new HashSet<>();
+        trie.match("teext", (int beginIndex, int endIndex, String payload) -> {
+            set.add(new ImmutableTriple<>(beginIndex, endIndex, payload));
+            return true;
+        });
+
+        assertTrue(set.isEmpty());
+    }
+
+    @Test
+    public void testMatchWholeString() {
+        TrieBuilder<String> builder = new TrieBuilder<>();
+        Trie<String> trie = builder.addCharSequence("text", "text")
+                .build();
+
+        Set<Triple<Integer, Integer, String>> set = new HashSet<>();
+        trie.match("text", (int beginIndex, int endIndex, String payload) -> {
+            set.add(new ImmutableTriple<>(beginIndex, endIndex, payload));
+            return true;
+        });
+
+        assertEquals(ImmutableSet.of(new ImmutableTriple<>(0, 4, "text")), set);
+    }
+
+    @Test
+    public void testMatchBeginningOfTheString() {
+        TrieBuilder<String> builder = new TrieBuilder<>();
+        Trie<String> trie = builder.addCharSequence("text", "text")
+                .build();
+
+        Set<Triple<Integer, Integer, String>> set = new HashSet<>();
+        trie.match("text1", (int beginIndex, int endIndex, String payload) -> {
+            set.add(new ImmutableTriple<>(beginIndex, endIndex, payload));
+            return true;
+        });
+
+        assertEquals(ImmutableSet.of(new ImmutableTriple<>(0, 4, "text")), set);
+    }
+
+    @Test
+    public void testMatchMiddleOfTheString() {
+        TrieBuilder<String> builder = new TrieBuilder<>();
+        Trie<String> trie = builder.addCharSequence("text", "text")
+                .build();
+
+        Set<Triple<Integer, Integer, String>> set = new HashSet<>();
+        trie.match("1text1", (int beginIndex, int endIndex, String payload) -> {
+            set.add(new ImmutableTriple<>(beginIndex, endIndex, payload));
+            return true;
+        });
+
+        assertEquals(ImmutableSet.of(new ImmutableTriple<>(1, 5, "text")), set);
+    }
+
+    @Test
+    public void testMatchEndOfTheString() {
+        TrieBuilder<String> builder = new TrieBuilder<>();
+        Trie<String> trie = builder.addCharSequence("text", "text")
+                .build();
+
+        Set<Triple<Integer, Integer, String>> set = new HashSet<>();
+        trie.match("1text", (int beginIndex, int endIndex, String payload) -> {
+            set.add(new ImmutableTriple<>(beginIndex, endIndex, payload));
+            return true;
+        });
+
+        assertEquals(ImmutableSet.of(new ImmutableTriple<>(1, 5, "text")), set);
+    }
+
+    @Test
+    public void testMatchTwice() {
+        TrieBuilder<String> builder = new TrieBuilder<>();
+        Trie<String> trie = builder.addCharSequence("text", "text")
+                .build();
+
+        Set<Triple<Integer, Integer, String>> set = new HashSet<>();
+        trie.match("begin text text end", (int beginIndex, int endIndex, String payload) -> {
+            set.add(new ImmutableTriple<>(beginIndex, endIndex, payload));
+            return true;
+        });
+
+        assertEquals(ImmutableSet.of(new ImmutableTriple<>(6, 10, "text"),
+                new ImmutableTriple<>(11, 15, "text")), set);
+    }
+
+    @Test
+    public void testMatchDifferentWords_NoOverlap() {
+        TrieBuilder<String> builder = new TrieBuilder<>();
+        Trie<String> trie = builder.addCharSequence("foo", "foo")
+                .addCharSequence("bar", "bar")
+                .build();
+
+        Set<Triple<Integer, Integer, String>> set = new HashSet<>();
+        trie.match("foobar", (int beginIndex, int endIndex, String payload) -> {
+            set.add(new ImmutableTriple<>(beginIndex, endIndex, payload));
+            return true;
+        });
+
+        assertEquals(ImmutableSet.of(new ImmutableTriple<>(0, 3, "foo"),
+                new ImmutableTriple<>(3, 6, "bar")),
+                set);
+    }
+
+    @Test
+    public void testMatchDifferentWords_Overlap() {
         TrieBuilder<String> builder = new TrieBuilder<>();
         Trie<String> trie = builder.addCharSequence("hers", "hers")
                 .addCharSequence("his", "his")
