90aebf1232c030f50af28898825ec2298620b114
ruslan.sverchkov <ruslan.sverchkov@corp.mail.ru>
2016-08-07 18:29:36 +0300

    create project structure

--- core/src/test/java/org/rsverchk/ahocorasick/TrieBenchmark.java
+++ core/src/test/java/org/rsverchk/ahocorasick/TrieBenchmark.java
@@ -1,6 +1,7 @@
 package org.rsverchk.ahocorasick;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Triple;
 import org.openjdk.jmh.Main;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
@@ -25,6 +26,7 @@ public class TrieBenchmark {
     public static class MyState {
 
         public Trie<String> trie;
+        public List<String> lines;
         public String textOf10Symbols;
         public String textOf100Symbols;
         public String textOf1000Symbols;
@@ -34,7 +36,7 @@ public class TrieBenchmark {
         @Setup(Level.Trial)
         public void setup() throws Exception {
             URL url = Thread.currentThread().getContextClassLoader().getResource("google-10000-english.txt");
-            List<String> lines = Files.readAllLines(Paths.get(url.toURI()));
+            lines = Files.readAllLines(Paths.get(url.toURI()));
             TrieBuilder<String> builder = new TrieBuilder<>();
             for (String s : lines) {
                 builder.addCharSequence(s, s);
@@ -60,8 +62,8 @@ public class TrieBenchmark {
     @Warmup(iterations = 10)
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public void testTextOf10Symbols(MyState state, Blackhole blackhole) {
-        test(state.trie, state.textOf10Symbols, blackhole);
+    public void testTrie_TextOf10Symbols(MyState state, Blackhole blackhole) {
+        testTrie(state.trie, state.textOf10Symbols, blackhole);
     }
 
     @Benchmark
@@ -70,8 +72,8 @@ public class TrieBenchmark {
     @Warmup(iterations = 10)
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public void testTextOf100Symbols(MyState state, Blackhole blackhole) {
-        test(state.trie, state.textOf100Symbols, blackhole);
+    public void testTrie_TextOf100Symbols(MyState state, Blackhole blackhole) {
+        testTrie(state.trie, state.textOf100Symbols, blackhole);
     }
 
     @Benchmark
@@ -80,8 +82,8 @@ public class TrieBenchmark {
     @Warmup(iterations = 10)
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public void testTextOf1000Symbols(MyState state, Blackhole blackhole) {
-        test(state.trie, state.textOf1000Symbols, blackhole);
+    public void testTrie_TextOf1000Symbols(MyState state, Blackhole blackhole) {
+        testTrie(state.trie, state.textOf1000Symbols, blackhole);
     }
 
     @Benchmark
@@ -90,8 +92,8 @@ public class TrieBenchmark {
     @Warmup(iterations = 10)
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public void testTextOf10000Symbols(MyState state, Blackhole blackhole) {
-        test(state.trie, state.textOf10000Symbols, blackhole);
+    public void testTrie_TextOf10000Symbols(MyState state, Blackhole blackhole) {
+        testTrie(state.trie, state.textOf10000Symbols, blackhole);
     }
 
     @Benchmark
@@ -100,17 +102,78 @@ public class TrieBenchmark {
     @Warmup(iterations = 10)
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.MICROSECONDS)
-    public void testTextOf50000Symbols(MyState state, Blackhole blackhole) {
-        test(state.trie, state.textOf50000Symbols, blackhole);
+    public void testTrie_TextOf50000Symbols(MyState state, Blackhole blackhole) {
+        testTrie(state.trie, state.textOf50000Symbols, blackhole);
     }
 
-    private void test(Trie<String> trie, String text, Blackhole blackhole) {
+    @Benchmark
+    @Fork(value = 1, warmups = 0)
+    @Measurement(iterations = 10)
+    @Warmup(iterations = 10)
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void testBruteForce_TextOf10Symbols(MyState state, Blackhole blackhole) {
+        testBruteForce(state.lines, state.textOf10Symbols, blackhole);
+    }
+
+    @Benchmark
+    @Fork(value = 1, warmups = 0)
+    @Measurement(iterations = 10)
+    @Warmup(iterations = 10)
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void testBruteForce_TextOf100Symbols(MyState state, Blackhole blackhole) {
+        testBruteForce(state.lines, state.textOf100Symbols, blackhole);
+    }
+
+    @Benchmark
+    @Fork(value = 1, warmups = 0)
+    @Measurement(iterations = 10)
+    @Warmup(iterations = 10)
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void testBruteForce_TextOf1000Symbols(MyState state, Blackhole blackhole) {
+        testBruteForce(state.lines, state.textOf1000Symbols, blackhole);
+    }
+
+    @Benchmark
+    @Fork(value = 1, warmups = 0)
+    @Measurement(iterations = 10)
+    @Warmup(iterations = 10)
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void testBruteForce_TextOf10000Symbols(MyState state, Blackhole blackhole) {
+        testBruteForce(state.lines, state.textOf10000Symbols, blackhole);
+    }
+
+    @Benchmark
+    @Fork(value = 1, warmups = 0)
+    @Measurement(iterations = 10)
+    @Warmup(iterations = 10)
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void testBruteForce_TextOf50000Symbols(MyState state, Blackhole blackhole) {
+        testBruteForce(state.lines, state.textOf50000Symbols, blackhole);
+    }
+
+    private void testTrie(Trie<String> trie, String text, Blackhole blackhole) {
         AtomicLong counter = new AtomicLong();
         trie.match(text, (int beginIndex, int endIndex, String payload) -> {
             counter.incrementAndGet();
             return true;
         });
         blackhole.consume(counter);
+    }
+
+    private void testBruteForce(List<String> lines, String text, Blackhole blackhole) {
+        for (String s : lines) {
+            int index = text.indexOf(s);
+            while (index != -1) {
+                blackhole.consume(index);
+                blackhole.consume(index + s.length());
+                index = text.indexOf(s, index + 1);
+            }
+        }
     }
 
 }
